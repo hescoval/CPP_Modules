6bafc620d7a3ae047ee70abafd36d4cb967e6b05
Hugo Alexandre Escoval Da Silva Vieira <hescoval@c1r1s3.42porto.com>
2024-08-24 07:01:26 +0100

    06 done

--- 42cpp/06/ex02/headers/Base.hpp
+++ 42cpp/06/ex02/headers/Base.hpp
@@ -5,19 +5,15 @@
 
 class Base
 {
-    private:
-        Base*   (Base::*funcArray[3])(void);
-
     public:
         Base();
         virtual ~Base();
-        Base*   generate(void);
-
-        Base*      make_A();
-        Base*      make_B();
-        Base*      make_C();
-        void    identify(Base* p);
-        void    identify(Base& p);
 };
+        Base*       make_A();
+        Base*       make_B();
+        Base*       make_C();
+        Base*       generate(void);
+        void        identify(Base* p);
+        void        identify(Base& p);
 
 #endif
--- 42cpp/06/ex02/main.cpp
+++ 42cpp/06/ex02/main.cpp
@@ -1,16 +1,88 @@
 #include "headers/general.hpp"
 #include "headers/Base.hpp"
+#include "headers/A.hpp"
+#include "headers/B.hpp"
+#include "headers/C.hpp"
+
+Base*       make_A()
+{
+    cout << GREEN "Created Derived class A" RESET << endl;
+    return new A();
+}
+
+Base*       make_B()
+{
+    cout << GREEN "Created Derived class B" RESET << endl;
+    return new B();
+}
+
+Base*       make_C()
+{
+    cout << GREEN "Created Derived class C" RESET << endl;
+    return new C();
+}
+
+Base*       generate(void)
+{
+    std::srand(time(0));
+    Base* (*funcArray[3])();
+
+    funcArray[0] = &make_A;
+    funcArray[1] = &make_B;
+    funcArray[2] = &make_C;
+    int random_value = std::rand() % 3;
+    return (*funcArray[random_value])();
+}
+
+void        identify(Base* p)
+{
+    if(dynamic_cast<A*>(p) != NULL)
+        cout << CYAN "Subclass A detected!" RESET << endl;
+    if(dynamic_cast<B*>(p) != NULL)
+        cout << CYAN "Subclass B detected!" RESET << endl;
+    if(dynamic_cast<C*>(p) != NULL)
+        cout << CYAN "Subclass C detected!" RESET << endl;
+}
+
+void        identify(Base& p)
+{
+    try
+    {
+        (void)dynamic_cast<A&>(p);
+        cout << CYAN "Subclass A detected!" RESET << endl;
+    }
+    catch(std::exception& e)
+    {
+
+    }
+    try
+    {
+        (void)dynamic_cast<B&>(p);
+        cout << CYAN "Subclass B detected!" RESET << endl;
+    }
+    catch(std::exception& e)
+    {
+
+    }
+    try
+    {
+        (void)dynamic_cast<C&>(p);
+        cout << CYAN "Subclass C detected!" RESET << endl;
+    }
+    catch(std::exception& e)
+    {
+
+    }
+}
 
 void simple_dynamic_cast_test()
 {
-    Base p;
+    Base *derived = generate();
 
-    Base *derived = p.generate();
-
-    derived->identify(*derived);
+    identify(*derived);
 
     cout << RED "\nPointer Test\n" RESET << endl;
-    derived->identify(derived);
+    identify(derived);
 
     delete derived;
 }
